7fe68836100b62b31923197b773f1f4a73d6fa9a
Samandar-Komilov <komilov.samandar07@gmail.com>
2025-07-09 21:03:27 +0500

    feat: starlette admin

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -1,28 +1,27 @@
-from pydantic import BaseModel
-from typing import Optional, List
-
 from datetime import datetime
 
-from app.schemas import TagListResponse
+from pydantic import BaseModel
+
+from app.schemas.tag import TagListResponse
 
 
 class BookCreate(BaseModel):
     name: str
-    description: Optional[str] = None
+    description: str | None = None
     isbn: str
-    cover: Optional[str] | None = None
+    cover: str | None | None = None
     page_count: int
     author_id: int
     category_id: int
     publisher_id: int
-    tags: List[int] | None = None
+    tags: list[int] | None = None
 
 
 class BookListResponse(BaseModel):
     id: int
     name: str
-    description: Optional[str] = None
+    description: str | None = None
     isbn: str
-    tags: List[TagListResponse] = None
+    tags: list[TagListResponse] = None
     created_at: datetime
     updated_at: datetime
